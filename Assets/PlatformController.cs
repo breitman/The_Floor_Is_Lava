f86cabec837729e2aeab981a0f2f34bdcd6c0296
Thomas <thomas.liu@vanderbilt.edu>
2019-02-20 15:02:04 +0100

    Scrolling background scene

---
+++ Assets/PlatformController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    private float useSpeed;
+    public float directionSpeed;
+    float origX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        origX = transform.position.x;
+        useSpeed = -directionSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(useSpeed * Time.deltaTime, 0, 0);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}

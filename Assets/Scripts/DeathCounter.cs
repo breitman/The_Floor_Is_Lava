6f8824dd8e7ecdd6a7b2df2c072f0f6ff12f5008
jeoratz <jeoratz@gmail.com>
2019-03-01 02:48:19 +0100

    Platform revamp and game over screen working

--- Assets/Scripts/DeathCounter.cs
+++ Assets/Scripts/DeathCounter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DeathCounter : MonoBehaviour
 {
@@ -13,30 +14,59 @@ public class DeathCounter : MonoBehaviour
     public Text deathCount;
     public Text scoreCount;
 
+    private static DeathCounter dcInstance;
+
 
     private void Awake()
     {
-        DontDestroyOnLoad(FindObjectOfType<Canvas>());
-        DontDestroyOnLoad(gameObject);
-        player = GameObject.FindWithTag("Player");
-        score = 0;
-        frameCount = 0;
+        if (SceneManager.GetActiveScene().name == "Scrolling Background_current")
+        {
+            DontDestroyOnLoad(FindObjectOfType<Canvas>());
+            DontDestroyOnLoad(FindObjectOfType<Camera>());
+
+            if(dcInstance == null)
+            {
+                DontDestroyOnLoad(this);
+                dcInstance = this;
+            }
+
+            player = GameObject.FindWithTag("Player");
+            score = 0;
+            frameCount = 0;
+
+        }
+        else
+        {
+            //GameObject.Find("DontDestroyOnLoad").active = false;
+        }
     }
 
     private void Update()
     {
-        score += Time.deltaTime;
-        if (frameCount % 15 == 0)
+
+        if(SceneManager.GetActiveScene().name == "Game_Over")
         {
-            
-            scoreCount.text = "Score: " + Mathf.Floor(score).ToString();
+            scoreCount.text = Mathf.Floor(score).ToString();
+            //GameObject.Find("GameManager").GetComponent<DeathCounter>().scoreCount.text = Mathf.Floor(score).ToString();
         }
-        ++frameCount;
+        else
+        {
+            score += Time.deltaTime;
+            if (frameCount % 15 == 0)
+            {
+
+                scoreCount.text = "Score: " + Mathf.Floor(score).ToString();
+            }
+            ++frameCount;
+        }
     }
 
     public void IncreaseDeaths()
     {
-        ++deaths;
-        deathCount.text = "Deaths: " + deaths.ToString();
+        if (SceneManager.GetActiveScene().name != "Game_Over")
+        {
+            ++deaths;
+            deathCount.text = "Deaths: " + deaths.ToString();
+        }
     }
 }
